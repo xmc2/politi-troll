dcc33b86f85db641fbe6169a653fca832a860dc9
Matt Cole <Your Actual Email>
2016-09-12 21:42:57 -0400

    Hiding API keys

--- myf.R
+++ myf.R
@@ -1,3 +1,20 @@
-myf <- function(...){
-        paste(...)
-}
+require(twitteR)
+
+rdmTweets <- searchTwitter('#test', n=500)
+
+df <- do.call("rbind", lapply(rdmTweets, as.data.frame))
+
+summary(df)
+
+head(df)
+
+df[1,]
+?searchTwitter
+# Note that the Twitter search API only goes back 1500 tweets (I think?)
+
+#Create a dataframe based around the results
+df <- do.call("rbind", lapply(rdmTweets, as.data.frame))
+#Here are the columns
+names(df)
+#And some example content
+head(df,3)
