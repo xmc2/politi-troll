663cce92537becabe278cdef84d53b9a9d091957
Matt Cole <Your Actual Email>
2016-10-03 10:34:33 -0400

    scraped emoji data

---
+++ emoji.r
@@ -0,0 +1,17 @@
+library(rvest)
+library(dplyr)
+
+
+table_url = "http://unicode.org/emoji/charts/full-emoji-list.html"
+population <- table_url %>%
+        read_html() %>%
+        html_nodes(xpath='/html/body/div[3]/table') %>%
+        html_table() %>% as.data.frame()
+
+head(population)
+
+# getting rid of weird headings in the middle of the dataframe...
+population <- population[!(population$X. == "№"), ]
+population <- select(population, -Chart, -Apple, -Goog., -Twtr., -One, -FBM, -Wind., -Sams., -GMail,
+                     -SB, -DCM, -KDDI, -Name)
+colnames(population)
