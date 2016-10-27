9b84f0bac43165e3e82119c2c1ea938bbf9cf342
matt cole <mattcole1093@gmail.com>
2016-10-27 18:01:32 -0400

    Update 7_Editing.R

--- rscripts/7_Editing.R
+++ rscripts/7_Editing.R
@@ -5,6 +5,9 @@ list.of.packages <- c("ggplot2", "Rcpp", "readr", "stringr", "dplyr", "tidytext"
   
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
+  
+  
+lapply(list.of.packages, library, character.only = TRUE)
 
 
 #### to obtain data we can either re generate
