79d11af7b4d537168233367fe97c10c3d4075480
matt cole <mattcole1093@gmail.com>
2016-10-27 19:06:20 -0400

    Update 3_gettingdata.R

--- rscripts/3_gettingdata.R
+++ rscripts/3_gettingdata.R
@@ -1,18 +1,18 @@
 require(twitteR); require(dplyr); require(tidytext); require(readr)
-source("rscripts/hidden.R")
-source("rscripts/get_tweets_now_func.R")
-source("rscripts/selected_trolls.R")
+source("rscripts/0_hidden.R")
+source("rscripts/1_get_tweets_now_func.R")
+source("rscripts/2_selected_trolls.R")
 
 # BEGIN
 
 hrc_tweets <- get_those_tweets_meow(sample_size = 150, term = '@hillaryclinton'); hrc_time <- Sys.time()
 djt_tweets <- get_those_tweets_meow(sample_size = 150, term = '@donaldtrump'); djt_time <- Sys.time()
 data <- dplyr::bind_rows(hrc_tweets, djt_tweets)
-write.csv(data, "outputs/data2.csv")
-x <- read.csv("outputs/data2.csv")
+write.csv(data, "outputs/data1.csv")
+x <- read.csv("outputs/data1.csv")
 x <- x %>% 
   select(screenName, text1, text2, text3, text4, text5, text6, text7, text8, text9, text10)
-write.csv(x, "outputs/classifyme1.csv")
+write.csv(x, "outputs/classifyme.csv")
 rm(x)
 # END
 # 
