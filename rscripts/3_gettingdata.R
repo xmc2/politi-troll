d738c978abef0e46b38de0f2555d842c7a68b6a7
matt cole <mattcole1093@gmail.com>
2016-10-27 18:57:41 -0400

    Update 3_gettingdata.R

--- rscripts/3_gettingdata.R
+++ rscripts/3_gettingdata.R
@@ -5,15 +5,16 @@ source("rscripts/selected_trolls.R")
 
 # BEGIN
 
-# hrc_tweets <- get_those_tweets_meow(sample_size = 150, term = '@hillaryclinton'); hrc_time <- Sys.time()
-# djt_tweets <- get_those_tweets_meow(sample_size = 150, term = '@donaldtrump'); djt_time <- Sys.time()
-# data <- dplyr::bind_rows(hrc_tweets, djt_tweets)
-# #write.csv(data, "outputs/data2.csv")
-# x <- read.csv("outputs/data2.csv")
-# x <- x %>% select(screenName, text1, text2, text3, text4, text5, text6, text7, text8, text9, text10)
-# #write.csv(x, "outputs/classifyme1.csv")
-# rm(x)
-# # END
+hrc_tweets <- get_those_tweets_meow(sample_size = 150, term = '@hillaryclinton'); hrc_time <- Sys.time()
+djt_tweets <- get_those_tweets_meow(sample_size = 150, term = '@donaldtrump'); djt_time <- Sys.time()
+data <- dplyr::bind_rows(hrc_tweets, djt_tweets)
+write.csv(data, "outputs/data2.csv")
+x <- read.csv("outputs/data2.csv")
+x <- x %>% 
+  select(screenName, text1, text2, text3, text4, text5, text6, text7, text8, text9, text10)
+write.csv(x, "outputs/classifyme1.csv")
+rm(x)
+# END
 # 
 # 
 # #####
@@ -23,8 +24,8 @@ source("rscripts/selected_trolls.R")
 # #####
 # 
 # #####
-# 
-# existing_trolls <- select(read.csv("outputs/selected_trolls.csv"), -X)
-# 
+
+existing_trolls <- select(read.csv("outputs/selected_trolls.csv"), -X)
+
 
 
